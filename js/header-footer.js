4261fe861ce43f7204d6b1d4885d6578219d33b0
kaori <klarkrheinonate@gmail.com>
2024-11-20 18:00:57 +0800

    doned

--- js/header-footer.js
+++ js/header-footer.js
@@ -17,3 +17,12 @@ function redirectToInstagram(){
 function redirectToTwitter(){
     window.location.assign(" ");
 }
+function igPost1(){
+    window.location.assign("https://www.instagram.com/p/DCjqT8xyMOC/?igsh=ZndjazZ6ZHV0b2Yz&fbclid=IwZXh0bgNhZW0CMTEAAR063upbWIFXa-3m5Yb6PB9A2QR-_cp2htlqh616--ViEL-aILoIr9DwP1s_aem_HU9rb0SdUKb7eUIXpWHrUg");
+}
+function igPost2(){
+    window.location.assign("https://www.instagram.com/p/DCjqGGRydBb/?igsh=ZDcwb2lnZzU0Y2kx&fbclid=IwZXh0bgNhZW0CMTEAAR0HcoSkwCyec9tOOvO7uYOnK9w3g7ddffZvid_87LMhqaJZfjPc8rqKbjA_aem_06QHfVDi85l_zn1lRIDe8w");
+}
+function igPost3(){
+    window.location.assign("https://www.instagram.com/p/DCjqKnWyOMJ/?igsh=MXkwcDBqMjdmY2huNw%3D%3D&fbclid=IwZXh0bgNhZW0CMTEAAR1hrkVa3nYUg0NeRmIAMIgNyLs0-0EvAIy7O9xNe0P-zqROI6RhVZXgPxk_aem__Agv1hYIRazAu3yVG84Yvw");
+}
